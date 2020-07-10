ff1e9cff1ddb9789256bd2bd2f48b729c6e90685
Plamena <plamenaxaskovo@gmail.com>
2020-07-11 01:19:50 +0300

    Add

---
+++ XML Converter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace XML_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the file with data
+            var path1 = Console.ReadLine();
+
+            // check if everything is all right with the file
+            ServiceabilityCheck(path1);
+
+            // the file to be processed
+            var path2 = Console.ReadLine();
+
+            // check if everything is all right with the file
+            
+            ZipFile.ExtractToDirectory(@path2, @"C:\Users\User\Desktop\Files");
+            string[] dirs = Directory.GetFiles(@"C:\Users\User\Desktop\Files\");
+            foreach (string dir in dirs)
+            {
+                ServiceabilityCheck(dir);
+            }
+        }
+
+        private static void ServiceabilityCheck(string path)
+        {
+            if (!(File.Exists(@path)))
+            {
+                throw new Exception("File does not exist.");
+            }
+
+            if (new FileInfo(@path).Length == 0)
+            {
+                throw new Exception("File is empty.");
+            }
+
+            string extension = Path.GetExtension(@path);
+            
+            if (extension != ".txt")
+            {
+                throw new Exception("The file isn’t in the correct format.");
+            }
+
+        }
+    }
+}
