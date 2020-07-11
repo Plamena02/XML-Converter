a1bb4b06203e25fb4427de36c1c6fd2b343fdb9a
Plamena <plamenaxaskovo@gmail.com>
2020-07-11 14:39:16 +0300

    Add

--- XML Converter/Program.cs
+++ XML Converter/Program.cs
@@ -29,23 +29,30 @@ namespace XML_Converter
 
         private static void ServiceabilityCheck(string path)
         {
-            if (!(File.Exists(@path)))
+            try
             {
-                throw new Exception($"The file with directory {path} does not exist.");
-            }
+                if (!(File.Exists(@path)))
+                {
+                    throw new Exception($"The file with directory {path} does not exist.");
+                }
 
-            if (new FileInfo(@path).Length == 0)
-            {
-                throw new Exception($"The file with directory {path} is empty.");
-            }
+                if (new FileInfo(@path).Length == 0)
+                {
+                    throw new Exception($"The file with directory {path} is empty.");
+                }
 
-            string extension = Path.GetExtension(@path);
+                string extension = Path.GetExtension(@path);
             
-            if (extension != ".txt")
-            {
-                throw new Exception($"The file with directory {path} is not in the correct format.");
+                if (extension != ".txt")
+                {
+                    throw new Exception($"The file with directory {path} is not in the correct format.");
+                }
             }
-
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
         }
     }
 }
