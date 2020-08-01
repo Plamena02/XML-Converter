04cb9986ead35fd06c680a61e3318041f63126c5
Plamena <plamenaxaskovo@gmail.com>
2020-08-01 14:47:44 +0300

    Add new methods

--- XML Converter/Program.cs
+++ XML Converter/Program.cs
@@ -4,6 +4,8 @@ using System.IO.Compression;
 using System.Linq;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
+using System.Dynamic;
+using System.Xml.Linq;
 
 namespace XML_Converter
 {
@@ -60,8 +62,73 @@ namespace XML_Converter
                     return (int)ExitCode.EXIT_ERROR;
                 }
             }
+            
+            var dataStores = DataProcessing(dirs, tagStore);
+            CreateXMLFiles(dataStores, workdir);
 
             return (int)ExitCode.EXIT_OK;
+        }
+
+        private static void CreateXMLFiles(List<DataStore> dataStores, string workdir)
+        {
+            foreach (var dataStore in dataStores)
+            {
+                XDocument XMLFile = new XDocument();
+                XMLFile.Add(new XComment($"<table name=\"{dataStore.FileName.Replace(".txt","")}\""));
+                var a = 1;
+                foreach (var dataList in dataStore.FileData)
+                {
+                    XMLFile.Add(new XComment($"<record id=\"{a}\">"));
+                    foreach (var data in dataList)
+                    {
+                        if (data.Definition != "")
+                        {
+                            var definition = data.Definition;
+                            var value = data.Value;
+                            XMLFile.Add(new XComment($"<{definition}>{value}</{definition}>"));
+                        }  
+                    }
+
+                    a++;
+                    XMLFile.Add(new XComment("</record>"));
+                }
+
+                XMLFile.Add(new XComment("</table>"));
+                XMLFile.Save(Path.ChangeExtension($@"{workdir}\Output", ".xml"));
+            }
+           
+        }
+
+        private static List<DataStore> DataProcessing(string[] dirs, TagStore tagStore)
+        {
+            var List = new List<DataStore>();
+            foreach (string dir in dirs)
+            {
+                DataStore dataStore = new DataStore(dir);
+                var fileNumber = tagStore.CheckFileName(dataStore.FileName);
+                if (fileNumber == -1)
+                {
+                    Console.WriteLine($"This file {dataStore.FileName} was not found.");
+                }
+                else
+                {
+                    foreach (var dataList in dataStore.FileData)
+                    {
+                        foreach (var item in dataList)
+                        {
+                            var tag = tagStore.CheckTag(fileNumber, Int32.Parse(item.Id));
+                            if (tag != null && tag.Length >= item.Value.Length)
+                            {
+                                item.Definition = tag.Definition;
+                            }
+                        }
+                    }
+                }
+
+                List.Add(dataStore);  
+            }
+            
+            return List;
         }
 
         private static bool CheckForFreeSpace(string filePath, string driveName)
