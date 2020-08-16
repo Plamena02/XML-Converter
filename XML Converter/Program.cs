7996d6fa1bafdf3a29da5bccdf70c8a2682f849e
Plamena <plamenaxaskovo@gmail.com>
2020-08-16 19:40:26 +0300

    Fix user messages

--- XML Converter/Program.cs
+++ XML Converter/Program.cs
@@ -14,7 +14,8 @@ namespace XML_Converter
 {
     class Program
     {
-        public enum ExitCode {
+        public enum ExitCode
+        {
             EXIT_OK = 0,
             EXIT_NO_PARAMETERS,
             EXIT_INVALID_FILES,
@@ -23,11 +24,12 @@ namespace XML_Converter
         };
 
         public static bool Warnings = false;
+        public static Dictionary<string, int> unknownTag = new Dictionary<string, int>();
 
         static int Main(string[] args)
         {
-            
-            Controller paramControl = new Controller(args);
+            var ar = Console.ReadLine().Split();
+            Controller paramControl = new Controller(ar);
             if (paramControl.NeedHelp())
             {
                 paramControl.ShowHelp();
@@ -41,7 +43,7 @@ namespace XML_Converter
 
             string config = paramControl.GetParam(ParamNames.config);
             TagStore tagStore = new TagStore(config);
-            if (tagStore.LoadFile() == false)           
+            if (tagStore.LoadFile() == false)
             {
                 return (int)ExitCode.EXIT_INVALID_FILES;
             }
@@ -49,12 +51,12 @@ namespace XML_Converter
             string archive = paramControl.GetParam(ParamNames.input);
             string workdir = paramControl.GetParam(ParamNames.output);
 
-            if (!(File.Exists(@archive))||!(Directory.Exists(@workdir))) 
+            if (!(File.Exists(@archive)) || !(Directory.Exists(@workdir)))
             {
                 return (int)ExitCode.EXIT_INVALID_FILES;
             }
 
-            workdir = Path.GetFullPath(workdir); 
+            workdir = Path.GetFullPath(workdir);
             var disk = $"{workdir[0]}{workdir[1]}{workdir[2]}";
 
             if (CheckForFreeSpace(@archive, disk) == false)
@@ -64,12 +66,18 @@ namespace XML_Converter
             }
 
             var StartTime = DateTime.Now;
-            Console.WriteLine($"Start Time:{ StartTime.ToString("h:mm:ss tt") } Config name:{Path.GetFileName(config)} Input file name:{Path.GetFileName(archive)} Output directory:{workdir}");
+            Console.WriteLine($"Start Time:{ StartTime.ToString("HH:mm:ss") }\nConfig name:{Path.GetFileName(config)} Input file name:{Path.GetFileName(archive)} Output directory:{workdir}\n");
 
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
 
-           ZipFile.ExtractToDirectory(@archive, $@"{workdir}",true);                
-           string[] dirs = Directory.GetFiles($@"{workdir}", "*.txt");            
-           var files = 0;
+            ZipFile.ExtractToDirectory(@archive, $@"{workdir}", true);
+            string[] dirs = Directory.GetFiles($@"{workdir}", "*.txt");
+            var files = 0;
+
+            stopWatch1.Stop();
+            var seconds = stopWatch1.Elapsed;
+            Console.WriteLine($"The files were unzipped for {Math.Round(seconds.TotalSeconds, 2)} seconds");
 
             foreach (string dir in dirs)
             {
@@ -85,16 +93,36 @@ namespace XML_Converter
                 }
                 else
                 {
-                    var DataFileName = Path.GetFileName(dir); 
+                    var DataFileName = Path.GetFileName(dir);
                     var fileNumber = tagStore.CheckFileName(DataFileName);
                     if (fileNumber == -1)
                     {
-                        Console.WriteLine($"This file {DataFileName} was not found.");
+                        Console.WriteLine($"WARNING: This file {DataFileName} was not found.");
                         Warnings = true;
                     }
 
                     string line;
                     System.IO.StreamReader file = new System.IO.StreamReader(@dir);
+
+                    string format = "yyyy-MM-dd HH:mm:ss";
+                    var date = File.ReadLines(@dir).First();
+                    var endLine = File.ReadLines(@dir).Last();
+
+                    try
+                    {
+                        DateTime dt = DateTime.ParseExact(date, format, CultureInfo.InvariantCulture);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"WARNING: First line is not in the correct format.");
+                        Warnings = true;
+                    }
+
+                    if (endLine.Contains("Datensaetze:") == false)
+                    {
+                        Console.WriteLine($"WARNING: Last line is not in the correct format.");
+                        Warnings = true;
+                    }
 
                     // create Xml File
                     var name = DataFileName.Replace(".txt", "");
@@ -107,12 +135,11 @@ namespace XML_Converter
 
                     xmlWriter.WriteStartElement("table");
                     xmlWriter.WriteAttributeString("name", name);
-                                        
-                    while ((line = file.ReadLine()) != null) 
-                    {
 
+                    while ((line = file.ReadLine()) != null)
+                    {
                         var arr = line.Split('|');
-                       
+
                         if (arr.Length > 1)
                         {
                             xmlWriter.WriteStartElement("record");
@@ -129,11 +156,11 @@ namespace XML_Converter
 
                                     if (tag != null)
                                     {
-                                        if (tag.Length >= value.Length) 
+                                        if (tag.Length >= value.Length)
                                         {
                                             try
                                             {
-                                                xmlWriter.WriteStartElement(tag.Definition);                                            
+                                                xmlWriter.WriteStartElement(tag.Definition);
                                                 xmlWriter.WriteString(value);
                                                 xmlWriter.WriteEndElement();
                                             }
@@ -141,58 +168,91 @@ namespace XML_Converter
                                             {
                                                 Console.WriteLine($"Unknown symbol was found on line {lines}");
                                             }
-                                                                                         
+
                                         }
                                         else
                                         {
-                                            Console.WriteLine($"Wrong value length. Expected length up to {tag.Length} characters, but was {value.Length}.");
+                                            Console.WriteLine($"WARNING: Wrong value length. Expected length up to {tag.Length} characters, but was {value.Length}.");
                                             Warnings = true;
                                         }
                                     }
                                     else
                                     {
-                                        Console.WriteLine($"Tag was not found on line{lines} with id {id}.");
+                                        if (!unknownTag.ContainsKey(id))
+                                        {
+                                            unknownTag.Add(id, 0);
+                                        }
+
+                                        unknownTag[id] += 1;
                                         Warnings = true;
                                     }
                                 }
-                            }                            
+                            }
                             xmlWriter.WriteEndElement();
                             lines++;
                         }
                     }
 
-                    xmlWriter.WriteEndElement(); 
-                    xmlWriter.WriteEndDocument(); 
-                 
-                    xmlWriter.Close();                   
-                    file.Close(); files++;                    
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.WriteEndDocument();
+
+                    xmlWriter.Close();
+                    file.Close(); files++;
                 }
-               
+
                 long InputLength = new System.IO.FileInfo(@dir).Length;
-                long OutputLength = new System.IO.FileInfo(Path.GetFullPath(FileName)).Length;
+                long OutputLength = new System.IO.FileInfo($@"{workdir}\{FileName}").Length;
                 stopWatch.Stop();
                 var sec = stopWatch.Elapsed;
 
-                Console.WriteLine($"{lines} lines processed in {Math.Round(sec.TotalSeconds,2)} seconds  Input file size: {ConvertBytesToMegabytes(InputLength)}MB  Output file size: {ConvertBytesToMegabytes(OutputLength)}MB"); 
+                Console.WriteLine($"{lines} lines processed in {Math.Round(sec.TotalSeconds, 2)} seconds  Input file size: {ConvertSize(InputLength)}  Output file size: {ConvertSize(OutputLength)}");
                 File.Delete(dir);
             }
 
             var EndTime = DateTime.Now;
             TimeSpan span = (EndTime - StartTime);
 
-            Console.WriteLine($"End time:{EndTime.ToString("h:mm:ss tt")} Files {files} processed in {span.Minutes} min {span.Seconds} seconds");
+            foreach (var item in unknownTag)
+            {
+                Console.WriteLine($"WARNING: Unknown tag {item.Key}, found {item.Value} times.");
+            }
+
+            Console.WriteLine($"\nEnd time:{EndTime.ToString("HH:mm:ss")}Files {files} processed in {span.Minutes} min {span.Seconds} seconds");
 
             if (!Warnings)
             {
-                return (int)ExitCode.EXIT_OK; 
+                return (int)ExitCode.EXIT_OK;
             }
 
             return (int)ExitCode.EXIT_WARNINGS;
         }
 
+        static string ConvertSize(long bytes)
+        {   
+            if (ConvertBytesToMegabytes(bytes) < 0.99)
+            {
+                return $"{ConvertBytesToKigabytes(bytes)} KB";
+            }
+            if (ConvertBytesToGigabytes(bytes) < 0.99)
+            {
+                return $"{ConvertBytesToMegabytes(bytes)} MB";
+            }
+            
+            return $"{ConvertBytesToGigabytes(bytes)} GB";
+        }
+
+        static double ConvertBytesToKigabytes(long bytes)
+        {
+            return Math.Round((bytes / 1024f), 2);
+        }
         static double ConvertBytesToMegabytes(long bytes)
         {
             return Math.Round(((bytes / 1024f) / 1024f), 2);
+        }
+
+        static double ConvertBytesToGigabytes(long bytes)
+        {
+            return Math.Round(((bytes / 1024f) / 1024f / 1024f), 2);
         }
 
         private static bool CheckForFreeSpace(string zipFile, string driveName)
@@ -203,7 +263,7 @@ namespace XML_Converter
                 foreach (ZipArchiveEntry entry in zip.Entries)
                 {
                     fileLengths += entry.Length;
-                }            
+                }
             }
 
             foreach (DriveInfo drive in DriveInfo.GetDrives())
@@ -215,36 +275,19 @@ namespace XML_Converter
                     {
                         return true;
                     }
-                }                  
+                }
             }
-            return false;  
+            return false;
         }
 
         private static bool ServiceAbilityCheck(string path)
         {
-            bool ret = true;
-
             if (!File.Exists(@path) || new FileInfo(@path).Length == 0)
             {
-                ret = false;
+                return false;
             }
 
-            string format = "yyyy-MM-dd HH:mm:ss"; 
-            var date = File.ReadLines(@path).First();
-            var endLine = File.ReadLines(@path).Last();
-
-            try
-            {
-               DateTime dt = DateTime.ParseExact(date, format, CultureInfo.InvariantCulture);
-            }
-            catch (Exception) { ret = false; }
-
-            if (endLine.Contains("Datensaetze:") == false)
-            {
-                ret = false;
-            }
-
-            return ret;
+            return true;
         }
     }
 }
